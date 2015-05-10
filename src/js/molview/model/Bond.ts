a71cce619c3ac06ec0018b19de951256b311f1c1
Eric Mulvihill <eric.mulvihill@gmail.com>
2015-05-10 09:32:35 -0700

    Add git submodule for DefinitelyTyped. Update copyright notice.

--- src/js/molview/model/Bond.ts
+++ src/js/molview/model/Bond.ts
@@ -2,7 +2,7 @@
  * =================================================================================================
  *
  * 	WebGL MolView
- * 	Copyright 2013 Eric Mulvihill. All Rights Reserved.
+ * 	Copyright 2013-2015 Eric Mulvihill. All Rights Reserved.
  *
  * 	This program is free software. You can redistribute and/or modify it
  * 	in accordance with the terms of the accompanying license agreement.
