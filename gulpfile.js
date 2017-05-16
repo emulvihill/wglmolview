e6f017b6208c89dfc705d4414177e08be21bd96b
Eric Mulvihill <eric.mulvihill@gmail.com>
2017-05-16 07:59:54 -0400

    add test dependencies, example test move ts files into /src

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,29 @@
-var gulp = require('gulp'),
-    connect = require('gulp-connect');
+"use strict";
 
-gulp.task('connect', function() {
-  connect.server({
-    root: 'app',
-    livereload: true
-  });
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var jasmineBrowser = require('gulp-jasmine-browser');
+
+gulp.task('jasmine', function () {
+    return gulp.src(['src/**/*.js', 'spec/**/*.spec.js'])
+        .pipe(jasmineBrowser.specRunner())
+        .pipe(jasmineBrowser.server({port: 8888}));
+});
+
+gulp.task('connect', function () {
+    connect.server({
+        root: 'app',
+        livereload: true
+    });
 });
 
 gulp.task('html', function () {
-  gulp.src('./app/*.html')
-      .pipe(connect.reload());
+    gulp.src('./app/*.html')
+        .pipe(connect.reload());
 });
 
 gulp.task('watch', function () {
-  gulp.watch(['./app/*.html'], ['html']);
+    gulp.watch(['./app/*.html'], ['html']);
 });
 
 gulp.task('default', ['connect', 'watch']);
