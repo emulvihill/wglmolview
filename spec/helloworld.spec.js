36258912dca5e6406bfb48a1c37b4ab94a9d8b9a
Eric Mulvihill <eric.mulvihill@gmail.com>
2017-05-18 14:43:56 -0400

    add spec file

---
+++ spec/helloworld.spec.js
@@ -0,0 +1,10 @@
+"use strict";
+describe('Hello world', function () {
+    it('says hello', function () {
+        expect(helloWorld()).toEqual("Hello world!");
+    });
+});
+
+function helloWorld() {
+    return "Hello world!";
+}
